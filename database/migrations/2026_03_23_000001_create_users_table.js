cd81f2f5dae2c107092c4c8bdb71459d1c19e263
Sruthiok <sruthi.tadiparti@gmail.com>
2026-03-23 13:33:18 +0530

    Create migration for users table

---
+++ database/migrations/2026_03_23_000001_create_users_table.js
@@ -0,0 +1,14 @@
+// Migration for users table
+exports.up = function(knex) {
+  return knex.schema.createTable('users', function(table) {
+    table.increments('id').primary();
+    table.string('username').notNullable();
+    table.string('password').notNullable();
+    table.string('email').notNullable().unique();
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
+};
